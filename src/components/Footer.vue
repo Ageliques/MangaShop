2a23c3f7ae9af9decd9ff7c1932a196b4148c2bf
Ageliques <131186569+Ageliques@users.noreply.github.com>
2023-05-19 03:38:45 +0700

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="footer-content">
+          <div class="logo-home"></div>
+          <div class="footer-text">
+            <div class="social-links">
+              <span class="social twitter"><a href="https://t.me/ageliques" target="_blank">Telegram</a></span>
+              <span class="social github"><a href="https://github.com/ageliques" target="_blank">Github</a></span>
+            </div>
+
+            <p>© 2023 MangaShop. Все права защищены.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  min-height: 300px;
+  text-align: center;
+  background-color: #111222;
+  padding-top: 120px;
+  font-size: 16px;
+  font-weight: 200;
+  color: #969696;
+  padding-right: 20px;
+  padding-bottom: 20px;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
